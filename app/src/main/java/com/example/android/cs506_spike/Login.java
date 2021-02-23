3496b5820273025a2b40180df8e79f8d89f1d507
monica22schmidt <monica22schmidt@gmail.com>
2021-02-23 01:16:04 -0600

    added login page and started customer view page

--- app/src/main/java/com/example/android/cs506_spike/Login.java
+++ app/src/main/java/com/example/android/cs506_spike/Login.java
@@ -2,7 +2,11 @@ package com.example.android.cs506_spike;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        EditText usernameIn = findViewById(R.id.usernameInput);
+        String username = usernameIn.getText().toString();
+        EditText passwordIn = findViewById(R.id.passwordInput);
+        String password = passwordIn.getText().toString();
+
+        // Check for valid input.
+        if (username != null && !username.equals("") && password != null && !password.equals("")) {
+            // Check if login credentials matches.
+            check(username, password);
+        } else {
+            Toast.makeText(Login.this, "Invalid input. Please try again!", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void check(final String username, final String password) {
+        //some checking credentials here
+        goToCustomerView(username);
+
+    }
+    // Redirect the user to main screen.
+    public void goToCustomerView(String username) {
+        // Redirect the user to main screen.
+        Intent intent = new Intent(this, CustomerView.class);
+        startActivity(intent);
+    }
+    public void goToRegister(View view) {
     }
 }

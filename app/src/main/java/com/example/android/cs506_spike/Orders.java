d7f9cfc6ca00f9ead0b2b8bb307480f99e31a9f7
Alvin Xiong <axiong37@wisc.edu>
2021-02-23 23:22:08 -0600

    -Alvin commits activity_orders.xml -Alvin commits Orders.java -Alvin commits activity_restaurant_staff.xml updates

---
+++ app/src/main/java/com/example/android/cs506_spike/Orders.java
@@ -0,0 +1,16 @@
+package com.example.android.cs506_spike;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Orders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders);
+    }
+
+}

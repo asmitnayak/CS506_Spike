4d887c405314013600644f19cac649f22e92785d
Alvin Xiong <axiong37@wisc.edu>
2021-02-25 17:51:01 -0600

    - Update Orders Page UI and script

--- app/src/main/java/com/example/android/cs506_spike/Orders.java
+++ app/src/main/java/com/example/android/cs506_spike/Orders.java
@@ -2,15 +2,29 @@ package com.example.android.cs506_spike;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Orders extends AppCompatActivity {
+    ArrayList<RestaurantMenuItem> menuItems = new ArrayList<RestaurantMenuItem>();
+    ArrayAdapter<RestaurantMenuItem> menuAdapter;
+    ListView ordersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+
+        ordersList = (ListView) findViewById(R.id.menuList);
+        RestaurantMenuItem [] pasta = new RestaurantMenuItem[10];
+        pasta[0] = new RestaurantMenuItem("Pasta", R.drawable.customer_view_background, 19.99, 9);
+        pasta[1] = new RestaurantMenuItem("Pasta", R.drawable.customer_view_background, 19.99, 9);
+        CustomAdapter customAdp = new CustomAdapter(getApplicationContext(), pasta);
+        ordersList.setAdapter(customAdp);
     }
 
 

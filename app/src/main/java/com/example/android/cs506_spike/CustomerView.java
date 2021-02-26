c3e4fce17a0783a688b5d7c1ba2f332b7e6e62bf
Asmit <asmit.nayak@gmail.com>
2021-02-26 16:44:48 -0600

    Created the menu file -> Customer view functionality

--- app/src/main/java/com/example/android/cs506_spike/CustomerView.java
+++ app/src/main/java/com/example/android/cs506_spike/CustomerView.java
@@ -18,7 +18,11 @@ import android.widget.Toast;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -35,10 +39,54 @@ public class CustomerView extends AppCompatActivity {
 
         menuList = (ListView) findViewById(R.id.menuList);
         foodItems = new ArrayList<RestaurantMenuItem>();
-        foodItems.add(new RestaurantMenuItem("Pasta", R.drawable.customer_view_background, 19.99, 9));
 
-        for(int i = 0; i < 10; i++)
-            foodItems.add(new RestaurantMenuItem("Pasta"+i, R.drawable.customer_view_background, 23.29, 6));
+        final File folder = getFilesDir();
+        File file = new File(folder, "cs506_spike");
+        if(!file.exists())
+            file.mkdir();
+        // TODO: Change filename
+        File mMenu = new File(file, "menu");
+
+        if (!mMenu.exists()) {
+                try (FileOutputStream stream = new FileOutputStream(mMenu, false)) {
+                    String tempMenu = "Pasta:drawable/customer_view_background:19.85:9";
+                    stream.write((tempMenu + "\n").getBytes());
+                } catch (Exception ignored) {
+                }
+        }
+
+        int length = (int) mMenu.length();
+        byte[] bytes = new byte[length];
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(mMenu);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            in.read(bytes);
+        } catch (IOException ignored){
+
+        }
+        finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String contents = new String(bytes);
+        String array[] = contents.split("\n");
+        for(String s : array){
+            String[] pieces = s.split(":");
+            foodItems.add(new RestaurantMenuItem(pieces[0], getResources().getIdentifier(pieces[1], null, getPackageName()), Double.parseDouble(pieces[2]), Integer.parseInt(pieces[3])));
+        }
+
+
+//        foodItems.add(new RestaurantMenuItem("Pasta", R.drawable.customer_view_background, 19.99, 9));
+//
+//        for(int i = 0; i < 10; i++)
+//            foodItems.add(new RestaurantMenuItem("Pasta"+i, R.drawable.customer_view_background, 23.29, 6));
         customAdp = new CustomAdapter(getApplicationContext(), foodItems);
         menuList.setAdapter(customAdp);
 
